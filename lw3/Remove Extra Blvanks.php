b9d6c4f50358929c5be224258b73eea5ee1aa10c
natural01 <80067153+natural01@users.noreply.github.com>
2021-03-05 18:16:13 +0300

    Update Remove Extra Blvanks.php

--- lw3/Remove Extra Blvanks.php
+++ lw3/Remove Extra Blvanks.php
@@ -1,2 +1,8 @@
-<? php
+<?php
+
+  header("Content-Type: text/plain");
   
+  $string = $_GET['text'];
+  $trimmed_str = trim($string);
+  $trimmed_str = preg_replace('/\s\s+/', ' ', $trimmed_str);
+  echo $trimmed_str;
